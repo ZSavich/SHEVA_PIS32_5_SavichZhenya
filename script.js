3bed20acf5c6c46e6a2d6481724c103aea255ffd
zsavich <zsavich59@gmail.com>
2019-04-01 23:17:45 +0300

    Выполнил третье задание

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
 "use strict";
 
-const btnRect = document.getElementById('getRect');
+const btnStart = document.getElementById('start');
 
-btnRect.addEventListener('click', createRectangle);
+btnStart.addEventListener('click', buttleGame);
 
-function createRectangle() {
-    const count = prompt('Введите кол-во строк');
-    let values = [];
-    let newValues = [];
+function buttleGame() {
+    let value = parseInt(prompt('Введите кол-во бутылок'));
+    let count = 0;
 
-    for(let i = 0; i < count; i++) {
-        let string = '';
+    while(value>1) {
+        value--;
+        count++;
 
-        for(let j = count; j > i; j--) {
-            string+='   ';
-        }
-        for(let j = 0; j < values.length-1; j++) {
-            newValues[j] = +values[j] + +values[j+1];
-        }
-        for(let j = 1; j < newValues.length+1; j++) {
-            values[j] = newValues[j-1];
-        }
-        values.push(1);
-        const numString = values.join('     ');
-        console.log(string+numString);
+        let first = `${value} бутыл${value===1? 'ка' : (value> 1 && value< 5? 'ки' : 'ок')} стоит на стене, `;
+        let second = `${counter(count)} упал${count===1?'а':'о'}`;
+
+        console.log(first+second);
     }
+}
 
+function counter(value) {
+    const number = value.toString().split('');
+    let string = '';
+
+    const once = ['', 'одна', 'две','три','четыре','пять','шесть','семь','восемь','девять','десять','одинадцать','двенадцать','тринадцать','четырнадцать','пятнадцать','шестнадцать','семнадцать','восемнадцать','девятнадцать'];
+    const ten = ['двадцать','тридцать','сорок','пятдесят','шестьдесят','семдесят','восемьдесят','дев\'яносто'];
+
+    if(value >= 10 && value < 20) {
+        return once[value];
+    }
+    for(let i = 0; i < number.length; i++) {
+        if(number[i] > 1 && number.length > 1 && i === 0)
+            string += `${ten[number[i]-2]}`;
+        else if (number.length > 1 && i === 0)
+            string += `${once[number[i]]} `;
+        if(number.length === 1)
+            string += once[number[i]];
+        if(number.length > 1 && i === 1) {
+            string += ` ${once[number[i]]}`;
+        }
+    }
+    return string;
 }
